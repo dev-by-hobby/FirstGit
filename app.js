70601142b62a7c2187b1b8078fb16e77ca121896
dev-by-hobby <subhadwip49943@gmail.com>
2023-09-05 21:14:28 +0530

    pushing the test code

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("I like Git");
+//console.dir(document);
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.textContent = 'Hello';
+headerTitle.style.border = 'solid 3px black';
+var titles = document.getElementsByClassName('title');
+console.log(titles);
+titles[0].style.fontWeight = 'bold';
+titles[0].style.color = '#6eeb34';
