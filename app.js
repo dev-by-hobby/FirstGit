0a3bdba5b993c903310869621a10489e969c392a
dev-by-hobby <subhadwip49943@gmail.com>
2023-09-05 22:42:39 +0530

    new li item get element

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var headerTitle = document.getElementById('header-title');
 console.log(headerTitle);
 headerTitle.textContent = 'Hello';
 headerTitle.style.border = 'solid 3px black';
-var titles = document.getElementsByClassName('title');
+var titles = document.getElementsByTagName('title');
 console.log(titles);
 titles[0].style.fontWeight = 'bold';
 titles[0].style.color = '#6eeb34';
-var lists = document.getElementsByClassName('list-group-item');
+var lists = document.getElementsByTagName('li');
 lists[2].style.background = '#34eb5e';
 for (let i = 0; i < lists.length; i++) {
     lists[i].style.fontWeight = 'bold';
