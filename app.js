210d4ca286ba3e7cd2c76852507ba718b256632b
dev-by-hobby <subhadwip49943@gmail.com>
2023-09-06 21:15:56 +0530

    dom manipulation 1

--- app.js
+++ app.js
@@ -17,10 +17,40 @@
 // var lists3 = document.querySelector('.list-group-item:nth-child(3)');
 // lists3.style.display = 'none';
 
-var listAll = document.querySelectorAll('.list-group-item');
-listAll[1].style.color ='#9bebb7';
+// var listAll = document.querySelectorAll('.list-group-item');
+// listAll[1].style.color ='#9bebb7';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(let i=0;i<odd.length;i++){
-odd[i].style.background = '#34eb5e';
-}
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for(let i=0;i<odd.length;i++){
+// odd[i].style.background = '#34eb5e';
+// }
+var itemList = document.querySelector('#items');
+
+console.log(itemList.parentElement);
+console.log(itemList.firstChild);
+console.log(itemList.firstElementChild);
+console.log(itemList.lastChild);
+itemList.lastElementChild.textContent="Seriously! This is the last one.";
+console.log(itemList.nextSibling);
+console.log(itemList.nextElementSibling);
+
+console.log(itemList.previousSibling);
+console.log(itemList.previousElementSibling);
+
+var newDiv = document.createElement('div');
+newDiv.className= "firstAdded";
+var newDivText = document.createTextNode('Hello');
+newDiv.appendChild(newDivText);
+// console.log(newDiv);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newDiv, h1);
+
+var newDiv2 = document.createElement('div');
+newDiv2.className= "secondAdded";
+var newDivText2 = document.createTextNode('Hello');
+newDiv2.appendChild(newDivText2);
+
+var unorder = document.querySelector('ul');
+var li1 = document.querySelector('ul li:first-child');
+unorder.insertBefore(newDiv2, li1);
