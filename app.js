324c804ad7ac336d6bae397df0c8dc94ef0a86e9
dev-by-hobby <subhadwip49943@gmail.com>
2023-09-08 22:46:24 +0530

    input working

--- app.js
+++ app.js
@@ -58,11 +58,15 @@ unorder.insertBefore(newDiv2, li1);
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');
+form.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 filter.addEventListener('keyup', filterItems);
 function addItem(e){
     e.preventDefault();
-  
+    var newItem = document.getElementById('item').value;
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));
     var deleteBtn = document.createElement('button');
   
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
